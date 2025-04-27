bc96c422a3e2d96591dc7b1cb8238552b6e8c697
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-27 07:19:51 +0530

    k8-eksctl

--- workstation.sh
+++ workstation.sh
@@ -64,10 +64,10 @@ eksctl version
 VALIDATE $? "eksctl installation"
 
 
-# kubens
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-VALIDATE $? "kubens installation"
+# # kubens
+# git clone https://github.com/ahmetb/kubectx /opt/kubectx
+# ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+# VALIDATE $? "kubens installation"
 
 
 # Helm
