3c65d97a52bb9e1e31549797bb3f33f1e25b4b1a
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-27 07:15:13 +0530

    k8-eksctl

--- workstation.sh
+++ workstation.sh
@@ -45,12 +45,24 @@ chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
 
+# # eksctl
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# sudo mv /tmp/eksctl /usr/local/bin
+# eksctl version
+# VALIDATE $? "eksctl installation"
+
 # eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
+if ! echo $PATH | grep -q "/usr/local/bin"; then
+    export PATH=$PATH:/usr/local/bin
+    echo 'export PATH=$PATH:/usr/local/bin' >> /etc/profile
+fi
 eksctl version
 VALIDATE $? "eksctl installation"
+
 
 # kubens
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
