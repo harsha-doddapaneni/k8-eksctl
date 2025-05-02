17b92a49dc882d571b6ce6c671188dc3b7d87c97
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-05-02 12:31:46 +0530

    k8

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
     backend "s3" {
-        bucket = "81s-remot-state"
+        bucket = "81s-remot-stat"
         key = "k8-eksctl"
         region = "us-east-1"
         dynamodb_table = "81s-locking"
