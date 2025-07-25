33215d9d5bf9db01d15c8e25897c7cb67ba78609
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-07-25 10:39:06 +0530

    k8

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "sg_id" {
   type        = string
-  default     = "sg-0fe656c7eda0c607b"
+  default     = "sg-0aba49a55dfe234b4"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = "subnet-006b392f9429ec396"
+    default = "subnet-0a5ff58531b1188c3"
 }
